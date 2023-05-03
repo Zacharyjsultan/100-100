9d8aa5a258a075221192cd654035a6a3578156cb
Zach <Zach@Zanzibars-Computer.local>
2023-05-03 09:20:06 -0700

    liftoff

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,11 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <button>
-        <img src="/logo1.png" alt="button" />
-      </button>
+      <a href="/Landing">
+        <button>
+          <img src="/logo1.png" alt="button" />
+        </button>
+      </a>
     </div>
   );
 }
