54b48ed591232250624b2621cb6e736956ac2d32
Zach <Zach@Zanzibars-Computer.local>
2023-05-20 13:01:10 -0700

    sticky fingers. im tired and need to code more

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,6 +4,7 @@ import "./Landing.css";
 export default function Landing() {
   return (
     <div className="landing">
+      <div></div>
       <div className="landing-box">gdfg</div>
     </div>
   );
